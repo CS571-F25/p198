1f2be1ce8cfcf8f0d129157498b2aa6107682119
Vincent Zhou <vzhou2@wisc.edu>
2025-11-05 22:53:09 -0600

    Fix base path to /p198/

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
 plugins: [react()],
-base: '/p198-Matchbites/',
+base: '/p198/',
 build: {
 outDir: 'docs'
 }
